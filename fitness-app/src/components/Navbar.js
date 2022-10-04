f6394d127f6dee829178d086e7c1ccd951f72f96
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-05 00:58:43 +0300

    navbar-components

--- fitness-app/src/components/Navbar.js
+++ fitness-app/src/components/Navbar.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Stack} from '@mui/material';
+import Logo from '../assets/images/Logo.png';
+import { color } from '@mui/system';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <>
+    <Stack 
+    direction="row"
+    justifyContent="space-around"
+    >
+      <Link to="/">
+        <img src={Logo} alt="Logo"  style={{width:"48px" ,margin:"0 20px"}}  />
+      </Link>
+      <Stack
+        direction="row"
+        gap="40px"
+        fontSize="40px"
+        alignItems="flex-end"
+>
+        <Link  to="/" style={
+          {textDecoration:"none" ,
+          color:"#3A1212",
+          borderBottom:"3px solid #FF625"
+        }
+        }>Home</Link>
+        <a href="#exercises" style={
+          {textDecoration:"none",
+          color:"#3A1212"
+          }}>Exercise</a>
+      </Stack>
+    </Stack>
+    
+    </>
+    )
 }
 
 export default Navbar
