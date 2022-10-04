a70ebde191f3d7141c77e972d0def31b3d3e3ae8
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-05 01:18:03 +0300

    update-Navbar

--- fitness-app/src/components/Navbar.js
+++ fitness-app/src/components/Navbar.js
@@ -7,8 +7,9 @@ import { color } from '@mui/system';
 function Navbar() {
   return (
     <>
-    <Stack 
+    <Stack
     direction="row"
+    gap="40px"
     justifyContent="space-around"
     sx={{gap:{sm:"122px" , xs:"40px"}, 
     mt:{sm:"32px",xs:"20px"},
@@ -16,27 +17,33 @@ function Navbar() {
   }} px="20px"
     >
       <Link to="/">
-        <img src={Logo} alt="Logo"  style={{width:"48px" ,margin:"0 20px"}}  />
+        <img src={Logo} alt="Logo" style={{width:"49px" ,height:"48px" ,margin:"0 20px" }} />
       </Link>
       <Stack
-        direction="row"
-        gap="40px"
-        fontSize="40px"
-        alignItems="flex-end"
->
-        <Link  to="/" style={
-          {textDecoration:"none" ,
-          color:"#3A1212",
-          borderBottom:"3px solid #FF625"
-        }
-        }>Home</Link>
-        <a href="#exercises" style={
-          {textDecoration:"none",
+      direction="row"
+      gap="40px"
+      fontSize="24px"
+      alignItems="flex-end"
+      >
+        <Link to="/" style={
+          {
+            textDecoration:"none",
+            color:"#3A1212",
+            borderBottom:"3px solid ##FF2625"
+          }}        
+          >
+          Home
+        </Link>
+        <a href="#exercises" style={{
+          textDecoration:"none",
           color:"#3A1212"
-          }}>Exercise</a>
+        }}
+        >
+          Exercise
+        </a>
       </Stack>
     </Stack>
-    
+
     </>
     )
 }
