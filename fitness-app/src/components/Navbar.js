33d5b5342adbce5dd04afe05a11a70a8365c7056
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-05 00:59:02 +0300

    add-responsive

--- fitness-app/src/components/Navbar.js
+++ fitness-app/src/components/Navbar.js
@@ -10,6 +10,10 @@ function Navbar() {
     <Stack 
     direction="row"
     justifyContent="space-around"
+    sx={{gap:{sm:"122px" , xs:"40px"}, 
+    mt:{sm:"32px",xs:"20px"},
+    justifyContent:"none"
+  }} px="20px"
     >
       <Link to="/">
         <img src={Logo} alt="Logo"  style={{width:"48px" ,margin:"0 20px"}}  />
