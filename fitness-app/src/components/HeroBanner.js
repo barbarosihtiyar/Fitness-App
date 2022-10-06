262a626c083efc96c94cb0b0a00204bdfd9187a5
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-06 23:18:35 +0300

    add-Component

---
+++ fitness-app/src/components/HeroBanner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Box , Stack , Typography } from '@mui/material';
+
+function HeroBanner() {
+  return (
+    <div>HeroBanner</div>
+  )
+}
+
+export default HeroBanner
