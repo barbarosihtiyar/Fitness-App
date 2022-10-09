bb2785cb3478d60e971b915d474e3d0ad7dc5c47
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-09 17:12:10 +0300

    created-task

--- fitness-app/src/components/SearchExercises.js
+++ fitness-app/src/components/SearchExercises.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {useEffect,useState} from 'react';
+import {Box,Button,Stack,TextField,Typography} from '@mui/material'
 
 function SearchExercises() {
   return (
-    <div>SearchExercises</div>
-  )
+    <Stack>
+      <Typography>
+      Awesome Exercises You <br/> Should Know
+      </Typography>
+      <Box>
+        <TextField />
+        <Button>
+
+        </Button>
+      </Box>
+    </Stack>
+
+    )
 }
 
 export default SearchExercises
