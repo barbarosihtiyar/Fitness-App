0acfda6d9ba669c3ab6f98ee80fe4b8696178027
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-09 17:27:10 +0300

    stack&Typography&TextField-add-css

--- fitness-app/src/components/SearchExercises.js
+++ fitness-app/src/components/SearchExercises.js
@@ -4,14 +4,32 @@ import {Box,Button,Stack,TextField,Typography} from '@mui/material'
 
 function SearchExercises() {
   return (
-    <Stack>
-      <Typography>
+    <Stack justifyContent="center" alignItems="center"  mt="37px" padding="20px">
+      <Typography fontWeight="700" mb="50px" textAlign="center"
+      sx={{
+        fontSize : {lg : "44px" , xs : "30px"}
+      }}
+      >
       Awesome Exercises You <br/> Should Know
       </Typography>
-      <Box>
-        <TextField />
+      <Box position="relative" mb="72px" height="76px" >
+       <TextField 
+      value="" 
+      onChange={(e) => {}}
+      placeHolder="Search Exercises"
+      type="text"
+      sx = {{
+        input : {fontWeight : "700" , 
+        border: "none" ,
+        borderRadius:"4px"
+      },
+      width : {lg : "800px" , xs  : "350px"},
+      backgroundColor : "#fff",
+      borderRadius : "40px"
+      }}
+      />
         <Button>
-
+        Search
         </Button>
       </Box>
     </Stack>
