e7e175718c9fee96d09081a1b9dcd6bf533ce70a
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-12 23:15:26 +0300

    datavaluesreturn&css

--- fitness-app/src/components/HorizontalScroollBar.js
+++ fitness-app/src/components/HorizontalScroollBar.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Box} from '@mui/material';
 
-function HorizontalScroollBar() {
+function HorizontalScroollBar({data}) {
   return (
-    <div>HorizontalScroollBar</div>
+    <div>
+        {data.map((item,index) => (
+        <Box 
+        key={index}
+        itemId={item.id || item}
+        title = {item.id || item}
+        m="0 40px"    
+        >
+            
+        </Box>
+        ))}
+    </div>
   )
 }
 
