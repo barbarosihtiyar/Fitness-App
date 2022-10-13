87acfbb7d6a52abd74b0f8c150a3ae251ed7f796
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-13 21:37:09 +0300

    add-useState

--- fitness-app/src/components/HorizontalScroollBar.js
+++ fitness-app/src/components/HorizontalScroollBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Box} from '@mui/material';
 
 function HorizontalScroollBar({data}) {
+  const [horizontal,setHorizontal] = useState("");
   return (
     <div>
         {data.map((item,index) => (
