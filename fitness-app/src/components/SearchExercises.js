0830f05ea36c06d319595ce13f831f1771800429
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-17 23:08:45 +0300

    sendComponent-bodyPart

--- fitness-app/src/components/SearchExercises.js
+++ fitness-app/src/components/SearchExercises.js
@@ -77,7 +77,7 @@ function SearchExercises() {
         </Button>
       </Box>
       <Box sx={{ position:"relative" , width:"100%", p:"20px"}}>
-        <HorizontalScrollBar data={bodyParts} />
+        <HorizontalScrollBar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         
       </Box>
     </Stack>
