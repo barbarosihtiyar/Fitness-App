b7090ad30f873c620ff3912c1b6b1febbfda4e58
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-18 22:08:29 +0300

    addProps

--- fitness-app/src/components/HorizontalScroollBar.js
+++ fitness-app/src/components/HorizontalScroollBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Box} from '@mui/material';
 import BodyPart from './BodyPart';
 
-function HorizontalScroollBar({data}) {
+function HorizontalScroollBar({data, bodyParts , setBodyPart}) {
 
   return (
     <div>
