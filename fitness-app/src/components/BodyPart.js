bd16938d482e767d3b9317ae1b629e41ba6e493d
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-24 22:02:21 +0300

    add-icon

--- fitness-app/src/components/BodyPart.js
+++ fitness-app/src/components/BodyPart.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import {Stack,Typography} from '@mui/material';
+
+import Icon from '../assets/icons/gym.png';
 function BodyPart() {
   return (
-    <div>BodyPart</div>
-  )
+<Stack>
+<img src={Icon} alt="dumbbell" className='body-part-icon'></img>
+</Stack>  
+)
 }
 
 export default BodyPart
