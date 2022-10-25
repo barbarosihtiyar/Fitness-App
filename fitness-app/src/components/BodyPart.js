beb56e938aabdeaba259086b11319612311004a1
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-25 21:59:06 +0300

    styleBodyPart-sx

--- fitness-app/src/components/BodyPart.js
+++ fitness-app/src/components/BodyPart.js
@@ -9,10 +9,19 @@ type="button"
 alignItems="center"
 justfyContent="center"
 className="bodyPart-card"
-sx={{
+sx={
+bodyPart === item ?  {
+  borderTop: "4px solid #ff2625",
+  backgroundColor:"#fff",
+  borderBottomLeftRadius:"20px",
+  width:"270px",
+  height:"280px",
+  cursor : "pointer" ,
+   gap:"47px"
 
+} : ""
 
-}}
+}
 
 >
 <img src={Icon} alt="dumbbell" className='body-part-icon' styles={{width:"40px" ,height:"40px"}} />
