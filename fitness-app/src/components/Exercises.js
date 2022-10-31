866f0a898c5c9ba339f8822dc772c450a2f30b52
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-10-31 18:20:22 +0300

    add-hooks-uimaterial-component

--- fitness-app/src/components/Exercises.js
+++ fitness-app/src/components/Exercises.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React , {useEffect,useState} from 'react';
+import Pagination fron '@mui/material/Pagination';
+import {Box, Stack , Typography} from '@mui/material';
+import {exerciseOptions,fetchData} from '../utils/fetchData';
 
-function Exercises() {
+function Exercises({exercises , setExercises , bodyPart}) {
   return (
     <div>Exercises</div>
   )
