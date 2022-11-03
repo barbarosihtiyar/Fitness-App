304223f8922e7c7356bec5e2eb55beec62d81120
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-11-03 22:17:40 +0300

    import-component

--- fitness-app/src/components/Exercises.js
+++ fitness-app/src/components/Exercises.js
@@ -2,6 +2,7 @@ import React , {useEffect,useState} from 'react';
 import Pagination fron '@mui/material/Pagination';
 import {Box, Stack , Typography} from '@mui/material';
 import {exerciseOptions,fetchData} from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({exercises,setExercises,bodyPart}) => {
   console.log(exercises);
