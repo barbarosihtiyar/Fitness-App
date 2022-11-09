c845feca7df9128438c94df62b899cdd27a0cc57
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-11-09 21:57:42 +0300

    lastExercise-fixedFunctionProblem-addNewVariable

--- fitness-app/src/components/Exercises.js
+++ fitness-app/src/components/Exercises.js
@@ -1,47 +1,53 @@
-import React , {useEffect,useState} from 'react';
-import Pagination fron '@mui/material/Pagination';
-import {Box, Stack , Typography} from '@mui/material';
-import {exerciseOptions,fetchData} from '../utils/fetchData';
-import ExerciseCard from './ExerciseCard';
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
-const Exercises = ({exercises,setExercises,bodyPart}) => {
-  const[currentPage,setCurrentPage] = useState(1);
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
   const exercisesPage = 9;
-}
 
-function Exercises({exercises , setExercises , bodyPart}) {
-  
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+
+  const indexOfFirstExercise = indexOfLastExercise - exercisePerPage;
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
   return (
-    <Box id="exercises"
-      sx={{mt:{lg:"110px"}}}
-      mt="50px"
-      p="20px"
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography varian="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
       >
-    <Typography varian="h3" mb="46px">
-    Showing Results
-    </Typography>
-    <Stack direction="row" sx={{gap:{lg:"110px",xs:"50px"}}}
-    flexWrap="wrap" justifyContent="center">
-    {exercises.map((exercise,index) => (
-      <ExerciseCard key={index} exercise={exercise} />
-    ))}
-    </Stack>
-    <Stack mt="100px" alignItems="center">
-      {exercises.length > 9 && (
-        <Pagination  
-           color="standard"
-           shape="rounded"
-           defaultPage={1}
-           count={Math.ceil(exercise.length / 
-           exercisesPage)}
-           page={currentPage}
-           onChange={paginate}
-           size="large"
-        />
-      )}
-    </Stack>
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
     </Box>
-  )
-}
+  );
+};
 
-export default Exercises
+export default Exercises;
