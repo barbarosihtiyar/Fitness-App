a5128952dc112ea1365c350f695677705909bc24
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-11-12 19:58:21 +0300

    import-hooks-library-components&addComponents

--- fitness-app/src/pages/ExerciseDetail.js
+++ fitness-app/src/pages/ExerciseDetail.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import { usePrams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 function ExerciseDetail() {
-  return (
-    <div>ExerciseDetail</div>
-  )
+  return;
+  <Box>
+    <Detail />
+    <ExerciseVideos />
+    <SimilarExercises />
+  </Box>;
 }
 
-export default ExerciseDetail
+export default ExerciseDetail;
