ada66f54bfab32317a8b50b4ebc8fb8299d06eb9
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-11-13 23:56:13 +0300

    createdComponent-importLibraries-

--- fitness-app/src/pages/ExerciseDetail.js
+++ fitness-app/src/pages/ExerciseDetail.js
@@ -3,11 +3,14 @@ import { useEffect, useState } from "react";
 import { usePrams } from "react-router-dom";
 import { Box } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercise";
 
 function ExerciseDetail() {
   return;
   <Box>
-    <Detail />
+    <Detail ExerciseDetail={ExerciseDetail} />
     <ExerciseVideos />
     <SimilarExercises />
   </Box>;
