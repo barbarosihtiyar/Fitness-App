ed38e9fba7a15ab9e5b8b13a1782942ce2b19d37
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-11-14 18:25:42 +0300

    fetchData-hooks-asyncFunction

--- fitness-app/src/components/HorizontalScroollBar.js
+++ fitness-app/src/components/HorizontalScroollBar.js
@@ -1,19 +1,14 @@
-import React , {useContext} from 'react';
-import {Box,Typography} from '@mui/material';
-import BodyPart from './BodyPart';
-import {ScroolMenu,visibilityContext} from 'react-horizontal-scrolling-menu';
-import BodyPart from './BodyPart';
+import React, { useContext } from "react";
+import { Box, Typography } from "@mui/material";
+import BodyPart from "./BodyPart";
+import { ScroolMenu, visibilityContext } from "react-horizontal-scrolling-menu";
 
-const HorizontalScroollBar =({data, bodyPart , setBodyPart}) =>   {
-
+const HorizontalScroollBar = ({ data, bodyPart, setBodyPart }) => {
   return (
     <ScroolMenu LeftArrow={LeftArrow} RigtArrow={RightArrow}>
-        
-            <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-        </Box>
-        ))}
+      <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
     </ScroolMenu>
-  )
-}
+  );
+};
 
-export default HorizontalScroollBar
+export default HorizontalScroollBar;
--- fitness-app/src/pages/ExerciseDetail.js
+++ fitness-app/src/pages/ExerciseDetail.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { usePrams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import Detail from "../components/Detail";
@@ -8,6 +8,24 @@ import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercise";
 
 function ExerciseDetail() {
+  const [ExerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+    };
+    fetchExerciseData();
+  }, [id]);
   return;
   <Box>
     <Detail ExerciseDetail={ExerciseDetail} />
