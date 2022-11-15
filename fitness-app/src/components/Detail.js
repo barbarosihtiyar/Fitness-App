2e450e14041b5235d682cf410c59f1e482dceb7e
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-11-15 16:24:59 +0300

    import-uiLibrary&images

--- fitness-app/src/components/Detail.js
+++ fitness-app/src/components/Detail.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { Typography, Stack, Button } from "@mui/icon/material";
+
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/body-part.png";
+import EquipmentImage from "../assets/icons/body-part.png";
 
 function Detail() {
   return <div>Detail</div>;
