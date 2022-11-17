c5f858bd6bf4af7a43f36cf1f39a50e334c1a58e
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-11-17 16:56:14 +0300

    extraInfo&styleComponent&createdArray

--- fitness-app/src/components/Detail.js
+++ fitness-app/src/components/Detail.js
@@ -7,6 +7,20 @@ import EquipmentImage from "../assets/icons/body-part.png";
 
 function Detail({ exerciseDetail }) {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+  ];
 
   return (
     <Stack
@@ -14,6 +28,21 @@ function Detail({ exerciseDetail }) {
       sx={{ flexDirection: { lg: "row" }, p: "20px", alingItems: "center" }}
     >
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography variant="h3">{name}</Typography>
+        {extraDetail.map((item) => (
+          <Stack key={item.name} direction="row" gap="24px" alingItems="center">
+            <Button>
+              <img src={item.icon} />
+            </Button>
+            <Typography variant="h6">
+              Exercises keep you strong.{name} {``}
+              is one of the best exercises to target your {target}.It will help
+              you improve your mood and gain energy.
+            </Typography>
+          </Stack>
+        ))}
+      </Stack>
     </Stack>
   );
 }
