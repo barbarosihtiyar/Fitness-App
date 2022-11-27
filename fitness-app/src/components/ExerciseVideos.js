397925ee40e1211cda8508deaeb94bf61cc97677
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-11-27 18:25:31 +0300

    adduiLibrary&adduiMaterial&styleComponent

--- fitness-app/src/components/ExerciseVideos.js
+++ fitness-app/src/components/ExerciseVideos.js
@@ -1,7 +1,14 @@
 import React from "react";
+import {Box,Stack,Typography} from '@mui/material';
 
-function ExerciseVideos() {
-  return <div>ExerciseVideos</div>;
+function ExerciseVideos({exerciseVideos,name}) {
+  return 
+  <Box sx={{marginTop:{lg:"200px" ,xs:"20px"}}} p="20px">
+  <Typography>
+
+  </Typography>
+
+  </Box>
 }
 
 export default ExerciseVideos;
