6ec424370082c18938563dae7b8eb69533fdb998
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-11-29 22:07:58 +0300

    adduiLibrary&styleComponent&adduiMaterial

--- fitness-app/src/components/SimilarExercise.js
+++ fitness-app/src/components/SimilarExercise.js
@@ -1,7 +1,17 @@
 import React from "react";
+imprt {Box,Stack,Typographt} from '@mui/material';
 
-function SimilarExercise() {
-  return <div>SimilarExercise</div>;
+function SimilarExercise({targetMuscleExercises,equipmentExercises}) {
+  return 
+  <Box sx={{nt:{lg:"100px",xs:"0"}}}>
+    <Typography variant="h3">Exercises that target the same muscle group
+    Exercises that target the same muscle group    
+    <Stack>
+      
+    </Stack>
+    </Typography>
+
+  </Box>
 }
 
 export default SimilarExercise;
