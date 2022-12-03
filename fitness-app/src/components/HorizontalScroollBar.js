f20289d9187ab4f8b66e09ed136005f7fb72c180
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-12-03 11:53:14 +0300

    import-Component&importImages

--- fitness-app/src/components/HorizontalScroollBar.js
+++ fitness-app/src/components/HorizontalScroollBar.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { Box, Typography } from "@mui/material";
 import BodyPart from "./BodyPart";
 import { ScroolMenu, visibilityContext } from "react-horizontal-scrolling-menu";
+import ExerciseCard from './ExerciseCard';
+import RightArrowIcon from './assets/right-arrow.png';
+import LeftArrowIcon from './assets/left-arrow.png';
 
 const HorizontalScroollBar = ({ data, bodyPart, setBodyPart }) => {
   return (
