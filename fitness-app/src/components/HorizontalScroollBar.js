7755db0a8e7471618190361b8e09ab204c7449ad
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-12-04 20:41:55 +0300

    mapItem&ımportComponent

--- fitness-app/src/components/HorizontalScroollBar.js
+++ fitness-app/src/components/HorizontalScroollBar.js
@@ -6,12 +6,20 @@ import ExerciseCard from './ExerciseCard';
 import RightArrowIcon from './assets/right-arrow.png';
 import LeftArrowIcon from './assets/left-arrow.png';
 
-const HorizontalScroollBar = ({ data, bodyPart, setBodyPart }) => {
+const HorizontalScroollBar = ({ data, bodyPart, setBodyPart , isBodyParts }) => {
   return (
     <ScroolMenu LeftArrow={LeftArrow} RigtArrow={RightArrow}>
-      <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-    </ScroolMenu>
+    {data.map((item) => (
+      <Box
+      key={item.id || item}
+      itemId={item.id || item}
+      title={item.id ||item}
+      m="0 40px"
+      >
+      {isBodyParts &&       <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}
+      {setBodyPart}   />}
   );
-};
+)};
+</ScroolMenu>
 
 export default HorizontalScroollBar;
