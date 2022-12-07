c48d17862b54d6692bd30228d13097bce3df1a01
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-12-08 00:07:50 +0300

    styleComponent&addText

--- fitness-app/src/components/Footer.js
+++ fitness-app/src/components/Footer.js
@@ -6,7 +6,10 @@ function Footer() {
   return (
     <Box mt="80px" bgcolor="#fff3f4">
     <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-     
+     <img src={Logo} alt="logo" width="200px" height="40px" />
+     <Typography variant="h5" pb="40px" mt="20px">
+      Fitness App
+     </Typography>
      </Stack>
     </Box>
   )
